c490c2f33ca8060c30949a814b97fb44b1eadef0
ManojStoic <manramsanbar@gmail.com>
2022-05-29 19:08:58 +0530

    Python day 10 exercise

---
+++ Python_Practice__Day10.py
@@ -0,0 +1,67 @@
+## Exercise - Github Day 10 - Q31
+"""Define a function which can print a dictionary where the keys 
+are numbers between 1 and 20 (both included) and the values are square of keys."""
+
+# def dicfunc():
+#     return {k:k**2 for k in range(1,21)}
+
+# print(dicfunc())
+
+## Exercise - Github Day 10 - Q32
+"""Define a function which can generate a dictionary where the keys
+ are numbers between 1 and 20 (both included) and the values are square of keys.
+ The function should just print the keys only."""
+
+# def dicfunc():
+#     dic = {k:k**2 for k in range(1,21)}
+#     return dic.keys()
+
+# print(dicfunc())
+
+## Exercise - Github Day 10 - Q33
+"""Define a function which can generate and print a list where the values are square of numbers between 1 and 20 (both included)."""
+
+# def lisfunc():
+#     return [x**2 for x in range(1,21)]
+
+# print(lisfunc())
+
+## Exercise - Github Day 10 - Q34
+"""Define a function which can generate a list where the values are square of numbers 
+between 1 and 20 (both included). Then the function needs to print the first 5 elements in the list."""
+
+
+# def lisfunc():
+#     return [x**2 for x in range(1,21)]
+
+# print(lisfunc()[:5])
+
+
+## Exercise - Github Day 10 - Q35
+"""Define a function which can generate a list where the values are 
+square of numbers between 1 and 20 (both included). Then the function needs to print the last 5 elements in the list."""
+
+
+# def lisfunc():
+#     return [x**2 for x in range(1,21)]
+
+# print(lisfunc()[-5:])
+
+
+## Exercise - Github Day 10 - Q36
+"""Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included).
+ Then the function needs to print all values except the first 5 elements in the list."""
+
+ 
+# def lisfunc():
+#     return [x**2 for x in range(1,21)]
+
+# print(lisfunc()[5:])
+
+## Exercise - Github Day 10 - Q37
+"""Define a function which can generate and print a tuple where the value are square of numbers between 1 and 20 (both included)."""
+
+# def lisfunc():
+#     return tuple([x**2 for x in range(1,21)])
+
+# print(lisfunc())
