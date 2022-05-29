00bfcbcbf745c2b952ed8e3d5c3b321a7aa1a97f
ManojStoic <manramsanbar@gmail.com>
2022-05-29 20:50:12 +0530

    Python Day 12 exercise

---
+++ Python_Practice_Day12.py
@@ -0,0 +1,36 @@
+## Exercise - Github Day 12 - Q44
+"""Write a program which can map() to make a list whose 
+elements are square of numbers between 1 and 20 (both included)."""
+
+# print(list(map(lambda x:x**2, range(1,21))))
+
+## Exercise - Github Day 12 - Q45
+"""Define a class named American which has a static method called printNationality."""
+
+# class American:
+#     @staticmethod
+#     def printNationality(name):
+#         return name
+
+# obj = American()
+# print(obj.printNationality("American"))
+# print(American.printNationality("American"))
+
+## Exercise - Github Day 12 - Q46
+"""Define a class named American and its subclass NewYorker."""
+
+# class American:
+#     #@staticmethod
+#     def printNationality(name):
+#         return name
+
+# class NeyYorker(American):
+
+#     def printNationality(self):
+#         return American.printNationality("NewYork")
+
+
+# # obj = American()
+# print(American.printNationality("American"))
+# obj1 = NeyYorker()
+# print(obj1.printNationality())
