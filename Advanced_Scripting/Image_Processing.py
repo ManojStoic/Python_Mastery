afa17e8e02bbdd0efe35957f960b9d9ebec9364d
ManojStoic <manramsanbar@gmail.com>
2022-05-31 12:44:24 +0530

    Image Processing

---
+++ Advanced_Scripting/Image_Processing.py
@@ -0,0 +1,17 @@
+# from PIL import Image,ImageFilter   
+
+# img = Image.open('./images/mauro-lima-ZWcg5MM-Mqk-unsplash.jpg')
+# img.thumbnail((200,200))
+# img.save('./images/lima_thumbnail.jpg')
+#filtered_Img = img.filter(ImageFilter.BLUR)
+#filtered_Img = img.convert('L')
+# img.save('./images/lima_test.png')
+# Rotated_Img = img.rotate(90)
+# Rotated_Resize_Img = Rotated_Img.resize((300, 300))
+# Rotated_Resize_Img.save('./images/pikachu_rotresize.png','png')
+# Rotated_Resize_Img.show()
+# box = (100,100,300,300)
+# region = img.crop(box)
+# region.save('./images/pikachu_crop.png','png')
+# print(img.size)
+
---
+++ Advanced_Scripting/JPGtoPNGconverter.py
@@ -0,0 +1,19 @@
+import sys
+import os
+from PIL import Image
+
+try:
+    sourcepath = sys.argv[1]
+    destinationpath =sys.argv[2]
+
+    if not os.path.exists(destinationpath): os.mkdir(destinationpath)
+
+    for files in os.listdir(sourcepath):
+        if '.jpg' in files:
+            img = Image.open(os.path.join(sourcepath,files))
+            img_name = os.path.splitext(os.path.join(destinationpath,files))[0]+'.png'
+            img.save(img_name)
+
+except Exception as err:
+    print(err.message)
+
